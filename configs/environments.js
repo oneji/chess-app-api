ece47003c923bd00acdd06494742464cdc59a7ac
Timur <timich1995@gmail.com>
2020-01-17 05:57:22 +0800

    FIX: Config file is changed.

--- configs/environments.js
+++ configs/environments.js
@@ -1,6 +1,6 @@
 module.exports = {
     development: {
-        db: 'mongodb://heroku_9837j3vp:vk6uf93654lhp7o39cgrs0vdru@ds263808.mlab.com:63808/heroku_9837j3vp',
+        db: 'mongodb://127.0.0.1/cueb-chess-db',
         port: process.env.PORT || 3010,
         secrets: {
             session: 'OneJI',
@@ -8,7 +8,7 @@ module.exports = {
         }
     },
     production: {
-        db: 'mongodb://127.0.0.1/cueb-chess-db',
+        db: 'mongodb://heroku_9837j3vp:vk6uf93654lhp7o39cgrs0vdru@ds263808.mlab.com:63808/heroku_9837j3vp',
         port: process.env.PORT || 3010,
         secrets: {
             session: 'OneJI',
